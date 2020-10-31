ba5d99d9413ada592319f641a186036d7940b022
EC2 Default User <ec2-user@ip-172-31-24-203.ec2.internal>
2020-10-31 08:35:29 +0000

    [add]modelを作成しました

---
+++ db/migrate/20201031081350_create_cart_items.rb
@@ -0,0 +1,11 @@
+class CreateCartItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cart_items do |t|
+      t.integer :customer_id
+      t.intger :item_id
+      t.integer :item_count
+
+      t.timestamps
+    end
+  end
+end
