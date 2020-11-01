e6aceb0771b561ef6b204c1b68fd652768d8947f
EC2 Default User <ec2-user@ip-172-31-24-203.ec2.internal>
2020-11-01 06:54:31 +0000

    [update]customerモデルのカラム追加、gem追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,10 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cart_items
+  has_many :orders
+  has_many :shipping_addresses
+  
+  
 end
