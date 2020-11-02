617e6e5c11b5cd79848927b2cf0869a86a3b8217
EC2 Default User <ec2-user@ip-172-31-33-50.ec2.internal>
2020-11-02 06:08:26 +0000

    [Add]ジャンル機能の追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,15 +1,35 @@
 class Admin::GenresController < ApplicationController
-    
-  def index
-  end
-  
-  def create
-  end
-  
-  def edit
-  end
-  
-  def update
-  end
-    
+
+    # before_action :authenticate_admin!
+
+    def index
+        @genre = Genre.new
+        @genres = Genre.all
+    end
+
+    def create
+        @genre = Genre.new(genre_params)
+        if @genre.save
+        redirect_to admin_genres_path
+        else
+          render 'index'
+        end
+    end
+
+    def edit
+        @genre= Genre.find(params[:id])
+    end
+
+    def update
+        @genre = Genre.find(params[:id])
+        @genre.update(genre_params)
+        redirect_to edit_admin_genre_path
+    end
+
+    private
+
+    def genre_params
+        params.require(:genre).permit(:name, :is_valid)
+    end
+
 end
