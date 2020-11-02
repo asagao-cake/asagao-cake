524f3775394e1f1ac92d7ea353d5e4444e0f8af3
EC2 Default User <ec2-user@ip-172-31-24-203.ec2.internal>
2020-11-02 09:07:40 +0000

    [修正]schemaファイルにも反映させました

--- db/migrate/20201102050027_change_orders_column.rb
+++ db/migrate/20201102050027_change_orders_column.rb
@@ -1,12 +1,12 @@
 class ChangeOrdersColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column_null :orders, :customer_id, null: false
-    change_column_null :orders, :billing_amount, null: false
-    change_column_null :orders, :shipping_fee, null: false
-    change_column_null :orders, :payment_method, null: false
-    change_column_null :orders, :postcode, null: false
-    change_column_null :orders, :address, null: false
-    change_column_null :orders, :name, null: false
-    change_column_null :orders, :status, null: false
+    change_column_null :orders, :customer_id, false
+    change_column_null :orders, :billing_amount,false
+    change_column_null :orders, :shipping_fee, false
+    change_column_null :orders, :payment_method, false
+    change_column_null :orders, :postcode, false
+    change_column_null :orders, :address, false
+    change_column_null :orders, :name, false
+    change_column_null :orders, :status, false
   end
 end
