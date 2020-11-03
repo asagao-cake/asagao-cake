8567178f7704d05b0fad68a50b6404e973b4b266
EC2 Default User <ec2-user@ip-172-31-56-52.ec2.internal>
2020-11-03 06:55:50 +0000

    [add]商品情報編集機能を追加しました。

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -22,6 +22,19 @@ class Admin::ItemsController < ApplicationController
      @items = Item.all
   end
 
+  def edit
+     @item = Item.find(params[:id])
+  end
+
+  def update
+     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render "edit"
+    end
+  end
+
 
   private
   def item_params
