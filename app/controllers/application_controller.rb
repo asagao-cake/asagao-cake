0bce4c9eb793d7f49fde8c70b5d022f73ca9753a
EC2 Default User <ec2-user@ip-172-31-56-52.ec2.internal>
2020-11-03 07:59:41 +0000

    [add]カート追加機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
 
+def current_cart
+    if session[:customer_id]
+      @cart_item = CartItem.find(session[:customer_id])
+    else
+      @cart_item = CartItem.create
+      session[:customer_id] = @customer.id
+    end
+end
+
   private
     def after_sign_in_path_for(admin)
         admin_path
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,19 @@
 class Public::CartItemsController < ApplicationController
+
+  def cart
+    @cart_items = current_customer.cart_items
+  end
+
+  def create
+    if @cart_item.blank?
+      @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
+    end
+    @cart_item.item_count += params[:item_count].to_i
+    @cart_item.save
+    redirect_to cart_item_path
+  end
+
+
+
+
 end
