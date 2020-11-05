08fbbfc60d3568c4897ed5ee5377848135445c1b
EC2 Default User <ec2-user@ip-172-31-71-26.ec2.internal>
2020-11-05 02:06:13 +0000

    ”[add]TOP,about画面作成”

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,14 @@
 class Public::HomesController < ApplicationController
+  def top
+    # オススメ商品
+    @recommended_items = Item.find(1,2,3,4)
+    #ジャンル検索
+    @genres = Genre.all
+    if @genre = Genre.find_by(name: params[:name])
+       @items = @genre.items.page(params[:page]).reverse_order
+    else
+       @items = Item.page(params[:page]).reverse_order
+    end
+  end
+
 end
