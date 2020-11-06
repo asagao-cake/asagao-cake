f48b1112526ac80568cb840a33132f6ef1372817
EC2 Default User <ec2-user@ip-172-31-24-203.ec2.internal>
2020-11-06 01:55:56 +0000

    [add]配送先の追加、編集、削除

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,2 +1,46 @@
 class Public::ShippingAddressesController < ApplicationController
+
+  def index
+    @address = ShippingAddress.new
+    @customer = Customer.find(current_customer.id)
+    @addresses = @customer.shipping_addresses
+  end
+
+  def create
+    @address = ShippingAddress.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to shipping_addresses_path
+    else
+      render :shipping_addresses_path
+    end
+  end
+
+  def edit
+    @address = ShippingAddress.find(params[:id])
+  end
+
+  def update
+    @address = ShippingAddress.find(params[:id])
+    if @address.update(address_params)
+      redirect_to shipping_addresses_path
+    else
+      render :edit_shipping_address_path
+    end
+  end
+
+
+  def destroy
+    @address = ShippingAddress.find(params[:id])
+    @address.destroy
+    redirect_to shipping_addresses_path
+  end
+
+
+private
+
+def address_params
+  params.require(:shipping_address).permit(:name, :address, :postcode)
+end
+
 end
