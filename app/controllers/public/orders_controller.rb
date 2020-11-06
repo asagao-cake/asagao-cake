9c71015e8e674356033869aeac975bca8569bc4c
EC2 Default User <ec2-user@ip-172-31-33-50.ec2.internal>
2020-11-06 02:24:42 +0000

    [Add]顧客側注文ページのHTML・CSSを追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,21 @@
 class Public::OrdersController < ApplicationController
+
+    def new #注文情報入力画面
+    end
+
+    def confirm #注文情報確認
+    end
+
+    def create #注文情報登録
+    end
+
+    def order_complete #注文完了
+    end
+
+    def index #注文履歴一覧
+    end
+
+    def show #注文履歴詳細
+    end
+
 end
