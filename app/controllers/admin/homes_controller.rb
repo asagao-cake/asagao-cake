f880f3757c5cd29e26347e3462db951c9c691c22
EC2 Default User <ec2-user@ip-172-31-24-203.ec2.internal>
2020-11-10 03:37:48 +0000

    [add]管理者側のTOPページを作成しました

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,2 +1,10 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
+
+
+  def top
+    range = Date.today.beginning_of_day..Date.today.end_of_day
+    @orders = Order.where(created_at: range)
+  end
+
 end
