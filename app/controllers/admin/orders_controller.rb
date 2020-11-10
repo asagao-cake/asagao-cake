8c9dcfe0ee04e4186fcc434ce18d2a7980d43862
EC2 Default User <ec2-user@ip-172-31-33-50.ec2.internal>
2020-11-10 06:07:27 +0000

    [Add]README.mdの作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,19 +7,14 @@ class Admin::OrdersController < ApplicationController
 
     def show #注文履歴詳細
         @order = Order.find(params[:id])
-<<<<<<< HEAD
         @order_items = OrderItem.where(order_id: params[:id])
         @total_fee = (@order.billing_amount - @order.shipping_fee).to_s(:delimited)
-=======
-        
->>>>>>> origin/develop
     end
 
     def update #注文ステータス
-             # 注文ステータス
-            @order_status = Order.find(params[:id])
-            @order_status.update(order_params)
-            redirect_to admin_order_path(params[:id])
+        @order_status = Order.find(params[:id])
+        @order_status.update(order_params)
+        redirect_to admin_order_path(params[:id])
     end
 
     def new
@@ -27,6 +22,7 @@ class Admin::OrdersController < ApplicationController
     end
 
     private
+
     def order_params
         params.require(:order).permit(:status)
     end
