8f2d827b626bd6991571874f7e56363e16e2eabb
EC2 Default User <ec2-user@ip-172-31-24-203.ec2.internal>
2020-11-11 01:56:23 +0000

    [modify]管理者headerの遷移先修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,14 +1,5 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
-  before_action :no_admin
-  
-  def no_admin(current_user)
-    case current_user
-    when Customer
-      redirect_to root_path
-      
-  end
-
 
   def top
     range = Date.today.beginning_of_day..Date.today.end_of_day
